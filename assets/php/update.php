c78d775410313f9b69eb988c6680da7b74c322a6
BlueBlizzardd <105523256+BlueBlizzardd@users.noreply.github.com>
2023-11-06 22:18:06 -0500

    Fixed update.php

--- assets/php/update.php
+++ assets/php/update.php
@@ -1,33 +1,29 @@
 <?php
-if (file_exists('php://input')) {
-    $post = file_get_contents('php://input');
-    $post = json_decode($post, true);
 
-    $path = '../json/data.json';
-    $data = file_get_contents($path);
-    $data = json_decode($data, true);
+$post = file_get_contents('php://input');
+$post = json_decode($post, true);
 
-    if (array_key_exists($post['id'] - 1, $data)) {
-        $data[$post['id'] - 1] = array(
-            "id" => $post["id"],
-            "activity" => $post["activity"],
-            "startDate" => $post["startDate"],
-            "endDate" => $post["endDate"],
-            "totalDays" => $post["totalDays"],
-            "initiator" => $post["initiator"]
-        );
-    }
+$path = '../json/data.json';
+$data = file_get_contents($path);
+$data = json_decode($data, true);
 
-    $data = json_encode($data, JSON_PRETTY_PRINT);
-
-    $fp = fopen($path, 'w');
-    fwrite($fp, $data);
-    fclose($fp);
-
-    echo $data;
-} else {
-    echo 'Error.';
+if (array_key_exists($post['id'] - 1, $data)) {
+    $data[$post['id'] - 1] = array(
+        "id" => $post["id"],
+        "activity" => $post["activity"],
+        "startDate" => $post["startDate"],
+        "endDate" => $post["endDate"],
+        "totalDays" => $post["totalDays"],
+        "initiator" => $post["initiator"]
+    );
 }
 
+$data = json_encode($data, JSON_PRETTY_PRINT);
+
+$fp = fopen($path, 'w');
+fwrite($fp, $data);
+fclose($fp);
+
+echo $data;
 
 ?>
